cfe69daf37bff9410fe267fc37fe967922bad7cd
ericaty <ericaty@au.to>
2023-12-01 15:54:18 +0800

    add value ref for div

--- django_madmin/__init__.py
+++ django_madmin/__init__.py
@@ -1,3 +1,3 @@
-__version_info__ = (1, 3, 8)
+__version_info__ = (1, 3, 9)
 __version__ = '.'.join([str(v) for v in __version_info__])
-__vue_version__ = 20231124153035
+__vue_version__ = 20231124180454
